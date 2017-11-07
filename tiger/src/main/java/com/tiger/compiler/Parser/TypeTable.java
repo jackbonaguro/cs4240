de7f8dfc2a29f28131e9c4770eaaae9a100c50fa
TJ Bright <tbright32@gatech.edu>
2017-11-06 20:06:58 -0500

    type table added

---
+++ tiger/src/main/java/com/tiger/compiler/Parser/TypeTable.java
@@ -0,0 +1,33 @@
+package com.tiger.compiler.parser;
+
+import java.util.*;
+import com.tiger.compiler.TypeTableEntry;
+
+public class TypeTable {
+  HashMap<Integer,TypeTableEntry> table;
+
+  public TypeTable() {
+  	this.table = new HashMap<Integer,TypeTableEntry>();
+  }
+
+  public void putEntry(int hash, TypeTableEntry entry) {
+
+  	for (TypeTableEntry hash : table.getValues()) {
+  	    
+  		if (hash.name.equals(entry.name)) {
+  			System.out.println("Cain't do dat");
+  			return;
+  		}
+  	}
+  	this.table.put(hash, entry);
+  }
+
+  public void readEntries() {
+  	for (Integer hash : table.keySet()) {
+            TypeTableEntry ent = table.get(hash);
+            System.out.print(hash + " : " + ent.id + ", ");
+            System.out.println(ent.name);
+        }
+
+  }
+}
