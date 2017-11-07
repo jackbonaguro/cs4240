b6a1090fd5b7a1c7ffac53ea83d653f2a8a614e7
TJ Bright <tbright32@gatech.edu>
2017-11-06 20:10:55 -0500

    table made to accomodate hashing

--- tiger/src/main/java/com/tiger/compiler/Parser/TypeTableEntry.java
+++ tiger/src/main/java/com/tiger/compiler/Parser/TypeTableEntry.java
@@ -2,12 +2,12 @@ package com.tiger.compiler.parser;
 
 import java.util.*;
 
-public class TypeTableEntry {
+public class TypeTableEntry extends Object{
 	public int id;
 	public String name;
 
-	// public TypeTableEntry(int id, String name) {
-	// 	this.id = id;
-	// 	this.name = name;
-	// }
+	public TypeTableEntry(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 }
