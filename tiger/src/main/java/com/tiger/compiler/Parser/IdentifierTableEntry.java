d62e33cb2c7599bbaca07cc42c6160227655ca25
TJ Bright <tbright32@gatech.edu>
2017-11-06 22:52:51 -0500

    removed undefined call from idtableentry

--- tiger/src/main/java/com/tiger/compiler/Parser/IdentifierTableEntry.java
+++ tiger/src/main/java/com/tiger/compiler/Parser/IdentifierTableEntry.java
@@ -6,7 +6,6 @@ public class IdentifierTableEntry extends Object{
     public int scope;
 
     public IdentifierTableEntry(String name, int typeId, int scope) {
-    	this.id = id;
         this.name = name;
         this.typeId = typeId;
         this.scope = scope;
