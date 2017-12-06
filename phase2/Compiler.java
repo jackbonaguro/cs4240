451cc8589411db80a8ea6474c879254fc5952aed
John Bonaguro <jbonaguro2@gmail.com>
2017-12-05 20:16:47 -0500

    Phase 2 Started

---
+++ phase2/Compiler.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
+public class Compiler{
+	public static Scanner scan;
+	public static ArrayList<String> file;
+	public static ArrayList<IROperation> iros;
+
+	public static void main(String[] args) {
+		try {
+			scan = new Scanner(new File(args[0]));
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("File Not Found");
+		}
+		file = new ArrayList<String>();
+		iros = new ArrayList<IROperation>();
+
+		while(scan.hasNextLine()) {
+			file.add(scan.nextLine());
+		}
+		//System.out.println(file);
+
+		for (String l: file) {
+			IROperation iro = new IROperation();
+			//First check for labels
+			if l.substring(l.length() - 2, l.length() - 1).equals(":") {
+				iro = new LabelOperation(l.substring(0, l.length() - 1));
+			}
+			//Otherwise split by arguments and find op name
+			else {
+				String[] line = l.split(",");
+				for (String i: line) {
+					i = i.trim();
+				}
+				switch (line[0]) {
+					case "assign":
+						if (line.length == 3) {
+							//Regular Assign
+							iro = new AssignOperation(line[1],line[2]);
+						}
+					break;
+				}
+			}
+
+			iros.add(iro);
+		}
+	}
+}
---
+++ phase2/LabelOperation.java
@@ -0,0 +1,7 @@
+public class LabelOperation implements IROperation {
+	public String name;
+
+	public LabelOperation(String name) {
+		this.name = name;
+	}
+}
